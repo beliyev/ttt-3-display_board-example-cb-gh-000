555e8dc2e11b368ff4044a4e416a4a3183e2b7b9
Ilkin Babayev <beliyev1@std.beu.edu.az>
2019-03-16 10:49:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Boardru
   def display_board
-    puts "A Tic Tac Toe Board"
+    print "A Tic Tac Toe Board"
   end
-  return display_board
